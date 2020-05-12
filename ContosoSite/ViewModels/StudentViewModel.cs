0d23a3fb27cb7dfdf47f1a6c1485ba3906013f7a
Mark Agaton <markallenagaton03@gmail.com>
2020-05-12 19:55:53 +0800

    Add project files.

---
+++ ContosoSite/ViewModels/StudentViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ContosoSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoSite.ViewModels
+{
+    public class StudentViewModel
+    {
+        public Student Student { get; set; }
+        public List<Enrollment> Enrollments { get; set; }
+    }
+}
